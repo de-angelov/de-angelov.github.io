23c7934732b0b48a9d8d5e7ac622479382421cda
denis <de.angelov.bg@gmail.com>
2024-06-17 23:02:23 +0000

    improvements

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
 function highlightCurrentPage() {
-        // Get the current URL path
         var currentPath = window.location.pathname.split('/').pop();
-        console.log("currentPath", currentPath);
-
         var selector = "#blog"
 
         if(currentPath == "cv.html") {
             selector = "#cv"
         }
 
-        if(currentPath == "index.html") {
+        if(currentPath == "" || currentPath == "index.html") {
             selector = "#home"
         }
 
         document
-        .querySelector(selector)
-        .classList
-        .add("header__nav--active")
+            .querySelector(selector)
+            .classList
+            .add("header__nav--active")
 }
 
-document.addEventListener("DOMContentLoaded", highlightCurrentPage)
+function initMermaidCharts(){
+    mermaid && mermaid.initialize({ startOnLoad: true });
+}
+
+document
+    .addEventListener(
+    "DOMContentLoaded", 
+    () => { 
+        highlightCurrentPage();
+        initMermaidCharts();
+    });
