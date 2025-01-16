812cbfd0771e3aa6a5ab4abe2d4523c7817cfef7
denis <de.angelov.bg@gmail.com>
2025-01-16 22:14:05 +0000

    style changes

--- js/index.js
+++ js/index.js
@@ -1,29 +1,75 @@
-function highlightCurrentPage() {
-        var currentPath = window.location.pathname.split('/').pop();
-        var selector = "#blog"
+(function () {
+	function highlightCurrentPage() {
+		var currentPath = window.location.pathname.split('/').pop();
+		var selector = "#blog"
 
-        if(currentPath == "cv.html") {
-            selector = "#cv"
-        }
+		if (currentPath == "cv.html") {
+			selector = "#cv"
+		}
 
-        if(currentPath == "" || currentPath == "index.html") {
-            selector = "#home"
-        }
+		if (currentPath == "" || currentPath == "index.html") {
+			selector = "#home"
+		}
 
-        document
-            .querySelector(selector)
-            .classList
-            .add("header__nav--active")
-}
+		document
+			.querySelector(selector)
+			.classList
+			.add("header__nav--active")
+	}
 
-function initMermaidCharts(){
-    mermaid && mermaid.initialize({ startOnLoad: true });
-}
+	function initMermaidCharts() {
+		mermaid && mermaid.initialize({ startOnLoad: true });
+	}
 
-document
-    .addEventListener(
-    "DOMContentLoaded", 
-    () => { 
-        highlightCurrentPage();
-        initMermaidCharts();
-    });
+	var filterParam = "filter";
+	var currentURL = new URL(window.location.href);
+
+	function handleFilterClick(e) {
+		
+		var filterValue = e.target.getAttribute('data-filter');
+		
+		console.log('click', e.target, filterValue);
+
+		var currentFilters = (currentURL.searchParams.get(filterParam) || "").split(',');
+		var updatedFilter = currentFilters.includes(filterValue)
+			? currentFilters.filter(function (x) { x !== filterValue })
+			: [...currentFilters, filterValue];
+
+		currentURL.searchParams.set('filter', updatedFilter.join(','));
+		hideShowFilters();
+	}
+
+	function hideShowFilters() {
+		var currentFilters = (currentURL.searchParams.get(filterParam) || "").split(',');
+		var currentBlogLinks = Array.from(document.querySelectorAll('.blog__article')) || [];
+
+		currentBlogLinks.forEach(function (a) {
+			var tags = a.attributes.getNamedItem('data-tags').split(',')
+			var hasFilter = currentFilters.some(x => tags.includes(x));
+
+			a.style.display = hasFilter ? 'none' : 'inherit';
+
+		});
+
+	}
+
+	function initFiltering() {
+		// init button filtering 
+
+		const buttons = Array.from(document.querySelectorAll('.tag-list__tag'));
+
+		buttons.forEach(function (b) { b.addEventListener('click', handleFilterClick) });
+
+	}
+
+	document
+		.addEventListener(
+			"DOMContentLoaded",
+			function () {
+				highlightCurrentPage();
+				initMermaidCharts();
+				initFiltering();
+				hideShowFilters();
+			});
+
+})();
